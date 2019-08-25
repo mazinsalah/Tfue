56e31afb1ddede03ee12477fb9fef4b9fb3aac19
mazinsalah <mazinsalah911@gmail.com>
2019-08-25 19:45:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ if (message.content.startsWith(prefix + 'setavatar')) {
 }
 
 if (message.content.startsWith(prefix + 'setT')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/peery13");
+  client.user.setGame(argresult, "https://www.twitch.tv/tfue");
      console.log('test' + argresult);
     message.channel.sendMessage(`Streaming: **${argresult}`)
 } 
